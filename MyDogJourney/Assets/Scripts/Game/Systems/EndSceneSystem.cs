e27453d3b59a3e1e63f0f259912d80e7223b308f
TeeLi888 <yigantee@outlook.com>
2024-07-07 12:21:13 +0800

    结束文案

--- MyDogJourney/Assets/Scripts/Game/Systems/EndSceneSystem.cs
+++ MyDogJourney/Assets/Scripts/Game/Systems/EndSceneSystem.cs
@@ -9,18 +9,25 @@ public class EndSceneSystem : TECSMonoSystem<EndSceneSystem>
     [SerializeField] private SpriteRenderer endImg;
     [SerializeField] private PlayerEntity player;
     [SerializeField] private CanvasGroup uiGroup;
+    [SerializeField] private CanvasGroup txtGroup;
 
     public void Play()
     {
         PlayerInputSystem.Inst.locked = true;
         player.Freeze();
 
-        TimingSystem.Inst.RunOnce("enddelay", 1f, () =>
-        {
-            uiGroup.DOFade(0, 1f);
-            endImg.gameObject.SetActive(true);
-            endImg.color = new Color(1f, 1f, 1f, 0f);
-            endImg.DOFade(1f, 5f).SetEase(Ease.Linear);
-        });
+        StartCoroutine(EndRoutine());
+    }
+
+    private IEnumerator EndRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+        uiGroup.DOFade(0, 1f);
+        endImg.gameObject.SetActive(true);
+        endImg.color = new Color(1f, 1f, 1f, 0f);
+        endImg.DOFade(1f, 5f).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(7f);
+        txtGroup.gameObject.SetActive(true);
+        txtGroup.DOFade(1f, 3f).SetEase(Ease.Linear);
     }
 }
