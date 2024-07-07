11874ceadc17be005dc0ef175478959818efab0f
TeeLi888 <yigantee@outlook.com>
2024-07-07 12:26:17 +0800

    心心动画

--- MyDogJourney/Assets/Scripts/Game/UI/HeartPanel/HeartFiller.cs
+++ MyDogJourney/Assets/Scripts/Game/UI/HeartPanel/HeartFiller.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,13 @@ public class HeartFiller : MonoBehaviour
     [SerializeField] private GameObject filler;
     public void SetFill(bool isOn)
     {
+        bool oldIsOn = gameObject.activeSelf;
         gameObject.SetActive(isOn);
+        if ((!oldIsOn) && isOn)
+        {
+            transform.DOKill();
+            transform.localScale = Vector3.zero;
+            transform.DOScale(Vector3.one, 0.2f).SetEase(Ease.Linear);
+        }
     }
 }
